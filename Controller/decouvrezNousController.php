bef55b32efd0e293b9ae59371aa39d137e01a502
Leok-isep <72254545+Leok-isep@users.noreply.github.com>
2021-05-29 16:06:16 +0200

    Delete Controller directory

--- Controller/decouvrezNousController.php
+++
@@ -1,5 +0,0 @@
-<?php session_start();
-include '../View/base/header.php';
-include '../View/base/decouvrezNous.php';
-include '../View/base/footer2.php';
-?>
--- Controller/faqController.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-
-
-
-include "../View/base/header.php";
-include "../View/base/footer.php";
-include '../Model/database.php';
-include '../View/base/faq.php';
-?>
